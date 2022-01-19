3939c0e43adb30552353c0a4cc58ff5a492e42fe
gregN <naudgr@gmail.com>
2022-01-19 17:10:36 +0100

    replication tailwind du design des cards manomano

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,34 @@ const Home = function Home() {
     <div className="text-red-500">
       <h1>Home page</h1>
       <Buttonturquoise />
+      {/* begin test design */}
+      <section className="p-8 gap-6 mx-auto flex items-center bg-lightgrey text-darkblue">
+        <div className="p-8 w-1/4 rounded-lg shadow-md bg-white border-1 border-lightgrey2">
+          <h1 className="text-h1 font-bold pb-4">Lorem ipsum dolor sit</h1>
+          <img className="w-32" src="./assets/images/placeholder.png" alt="" />
+          <p className="p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="p-8 w-1/4 rounded-lg shadow-md bg-white border-1 border-lightgrey2">
+          <h1 className="text-h1 font-bold pb-4">Lorem ipsum dolor sit</h1>
+          <img
+            className="w-32"
+            src="../../src/assets/images/placeholder.png"
+            alt=""
+          />
+          <p className="p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+      {/* end test design */}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFF",
+        lightgrey: "#F5F6F7",
+        lightgrey2: "#ecedf0",
+        teal: "#29b9ad",
+        darkblue: "#0C193A",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      fontWeight: {
+        light: 300,
+        regular: 400,
+        semibold: 600,
+        bold: 700,
+      },
+      fontSize: {
+        mini: "12px",
+        normal: "16px",
+        h1: "20px",
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
+  plugins: [],
 };
