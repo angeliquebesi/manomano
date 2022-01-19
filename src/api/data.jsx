f4615aca823d7d7c20d415bf096e21b8d2159883
angeliquebesi <besineaud85@gmail.com>
2022-01-19 19:00:31 +0100

    ajout de données

---
+++ src/api/data.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable import/prefer-default-export */
+import corvette from "../assets/images/Baignoires/corvette.jpg";
+import diva from "../assets/images/Baignoires/diva.jpg";
+import bastia from "../assets/images/Baignoires/bastia.jpg";
+import Intro from "../assets/images/Baignoires/Intro.jpg";
+import parebaignoire from "../assets/images/Accessoires/pare-baignoire.jpg"
+
+export const categories = [
+  "Meuble de salle de bain",
+  "Douche et accessoires",
+  "Baignoire et accessoires",
+  "Lavabos et vasque",
+  "Carrelage, béton ciré, et revêtement décoratif",
+];
+
+export const baignoires = [
+  {
+    id: 0,
+    nom: "Baignoire Corvette",
+    dimension: "160 x 70",
+    prix: "141,82 €",
+    image: corvette,
+  },
+  {
+    id: 1,
+    nom: "Baignoire Diva",
+    dimension: "160 x 70",
+    prix: "177,00 €",
+    image: diva,
+  },
+  {
+    id: 2,
+    nom: "Baignoire Bastia",
+    dimension: "170 x 75",
+    prix: "189,90 €",
+    image: bastia,
+  },
+  {
+    id: 3,
+    nom: "Baignoire Intro",
+    dimension: "160 x 75",
+    prix: "139,00 €",
+    image: Intro,
+  },
+];
+
+export const Accessoires = [
+  {
+    id : 0,
+    nom : "Pare baignoire",
+    description : "Pare baignoire 90x140cm pivotant 180degré",
+    prix : "101,36 €",
+    image : parebaignoire,
+  },
+  {
+    id : 1,
+    nom :
+    description :
+    prix :
+    image :
+  },
+  {
+    id : 2,
+    nom :
+    description :
+    prix :
+    image :
+  },
+  {
+    id : 3,
+    nom :
+    description :
+    prix :
+    image :
+  },
+  {
+    id : 4,
+    nom :
+    description :
+    prix :
+    image :
+  },
+]
