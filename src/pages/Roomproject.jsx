a9de39c8c9c0a3770684c81f754baf151d790922
LeonieNollevalle <leonie.nollevalle@gmail.com>
2022-01-20 13:40:57 +0100

    ajustement /room

--- src/pages/Roomproject.jsx
+++ src/pages/Roomproject.jsx
@@ -3,7 +3,7 @@ import Cardcategory from "../components/Cards/Cardcategory";
 
 function Roomproject() {
   return (
-    <div>
+    <div className=" bg-darkgrey text-lightgrey3 ">
       {/* Mettre le plan de la pièce Léonie */}
       <h2 className="text-ellipsis text-3xl overflow-hidden w-full h-8 font-bold text-center">
         Which elements ?
@@ -11,7 +11,7 @@ function Roomproject() {
       <p className="text-ellipsis text-xl w-full h-5 text-center">
         Click on ↑ Or search ↓
       </p>
-      <div className="text-black bg-white flex items-center justify-center mt-3">
+      <div className="text-black bg-darkgrey flex items-center justify-center mt-3">
         <div className="border rounded-3xl overflow-hidden flex">
           <input
             type="text"
