01a8ea0b1e0e4dc1da62ac65afa34fcf24f9ffe2
gregN <naudgr@gmail.com>
2022-01-20 20:34:51 +0100

    saving this page from oblivion

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img404 from "../assets/images/img404.gif";
 
 const Page404 = function Page404() {
   return (
-    <div>
-      <h1>Page 404</h1>
+    <div className="w-screen h-screen text-white bg-darkgrey flex flex-col">
+      <div className="mt-24 text-center font-bold">Error</div>
+      <h1 className="text-center text-7xl font-semibold">404</h1>
+      <img src={img404} alt="" className="p-4" />
+      <div className="mx-auto">
+        <Link to="/">
+          <button
+            className="transition px-6 mx-1 h-12 rounded bg-teal "
+            type="button"
+          >
+            take me home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
