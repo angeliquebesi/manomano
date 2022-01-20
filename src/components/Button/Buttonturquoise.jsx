8d94d4b29df75f7e464fd839f6d0027974c7b9f2
LeonieNollevalle <leonie.nollevalle@gmail.com>
2022-01-20 13:24:22 +0100

    creation structure jsx houseproject.jsx/creation houseproject.css/creation animation d'entrée

--- src/components/Button/Buttonturquoise.jsx
+++ src/components/Button/Buttonturquoise.jsx
@@ -5,7 +5,7 @@ function Buttonturquoise({ content }) {
   return (
     <div>
       <button
-        className="transition h-12 w-50 rounded bg-teal px-16 font-semibold hover:bg-teal-400 active:bg-teal-700  text-white"
+        className="transition h-12 rounded bg-teal px-16 font-semibold hover:bg-teal-400 active:bg-teal-700  text-white"
         type="button"
       >
         {content}
