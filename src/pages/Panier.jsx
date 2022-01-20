8da1885c231c12e86cf5716d5907defba9dc472b
gregN <naudgr@gmail.com>
2022-01-20 16:44:53 +0100

    remise en forme avec data

--- src/pages/Panier.jsx
+++ src/pages/Panier.jsx
@@ -1,52 +1,26 @@
 import React from "react";
 import Buttonturquoise from "../components/Button/Buttonturquoise";
+import corvette from "../assets/images/Baignoires/corvette.jpg";
 
 function Panier() {
   return (
-    <div className="w-screen h-screen text-white bg-darkgrey flex flex-col">
+    <div className="text-white bg-darkgrey flex flex-col">
       <h1 className="mt-20 mb-10 text-center text-h1 font-semibold">
         Your order :
       </h1>
       <section className="grid grid-rows-5 grid-flow-col w-full justify-around gap-4">
-        <div className="text-h2">
-          <span className="">something is here </span>
-          <span className="px-8">50€</span>
-          <button
-            className="transition h-6 w-6 rounded bg-teal font-semibold hover:bg-teal-400 active:bg-teal-700  text-white text-normal"
-            type="button"
-          >
-            X
-          </button>
-        </div>
-        <div className="text-h2">
-          <span className="">something is here </span>
-          <span className="px-8">50€</span>
-          <button
-            className="transition h-6 w-6 rounded bg-teal font-semibold hover:bg-teal-400 active:bg-teal-700  text-white text-normal"
-            type="button"
-          >
-            X
-          </button>
-        </div>
-        <div className="text-h2">
-          <span className="">something is here </span>
-          <span className="px-8">50€</span>
-          <button
-            className="transition h-6 w-6 rounded bg-teal font-semibold hover:bg-teal-400 active:bg-teal-700  text-white text-normal"
-            type="button"
-          >
-            X
-          </button>
-        </div>
-        <div className="text-h2">
-          <span className="">something is here </span>
-          <span className="px-8">50€</span>
-          <button
-            className="transition h-6 w-6 rounded bg-teal font-semibold hover:bg-teal-400 active:bg-teal-700  text-white text-normal"
-            type="button"
-          >
-            X
-          </button>
+        <div className="flex">
+          <img className="h-32" src={corvette} alt="" />
+          <div className="flex flex-col text-h2 pl-14 items-end">
+            <span className="text-normal ">Baignoire Corvette</span>
+            <span className="text-h1 font-bold">141,82 €</span>
+            <button
+              className="transition mt-9 h-6 w-6 rounded bg-teal font-semibold hover:bg-teal-400 active:bg-teal-700  text-white text-normal"
+              type="button"
+            >
+              X
+            </button>
+          </div>
         </div>
       </section>
       <div className="border-t-2 border-t-white w-10/12 mx-auto" />
